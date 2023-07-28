18a9bde300ce5906d52bc4850509f8a43533c003
gitcore33 <erickestanislau25@gmail.com>
2023-07-28 17:20:51 -0300

    creating sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,15 @@
+import avatar from '../img/perfil.jpeg'
+import '../styles/components/sidebar.sass'
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside id="sidebar">
+        <p>imagem</p>
+        <p className="title">Desenvolvedor</p>
+        <p>Redes sociais</p>
+        <p>Informações de contato</p>
+        <a href="" className="btn">Download do currículo</a>
+    </aside>
   )
 }
 
