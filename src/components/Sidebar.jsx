95ecdf40ecf13ccfc1e013fa123fbe7188f5cb9d
gitcore33 <erickestanislau25@gmail.com>
2023-07-29 21:40:55 -0300

    adding socials component and style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import avatar from '../img/perfil.jpeg'
 import '../styles/components/sidebar.sass'
+import SocialNetworks from './SocialNetworks'
+
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
         <img src={avatar} alt="Erick Estanislau" />
         <p className="title">Desenvolvedor</p>
-        <p>Redes sociais</p>
+        <SocialNetworks/>
         <p>Informações de contato</p>
         <a href="" className="btn">Download do currículo</a>
     </aside>
