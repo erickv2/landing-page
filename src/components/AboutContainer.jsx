a99c1d36c11eed1df1b22564bb68d0a9d74b4308
gitcore33 <erickestanislau25@gmail.com>
2023-07-29 22:21:50 -0300

    adding about and technologies container and style

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,14 @@
+
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+        <h2>Sobre:</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae nulla natus illo, laboriosam fugiat nihil ipsam, temporibus dolore autem quasi inventore distinctio quae modi optio laborum laudantium nemo officiis magnam!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At voluptates incidunt ut dolore voluptatem magni nostrum quaerat mollitia repudiandae praesentium doloremque veritatis rerum, repellat, quia aperiam quis maiores dicta tenetur!</p>
+
+        
+    </section>
+  )
+}
+
+export default AboutContainer
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,14 @@
+
+const ProjectsContainer = () => {
+  return (
+    <section id="projects-container">
+        <h2>Projetos:</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur labore vel quo soluta fugit ipsa aliquam incidunt error asperiores architecto. Tempora amet a hic quae vero, aut exercitationem est doloremque.</p>
+        <a href="#" className="btn">
+            Ver Projetos
+        </a>
+    </section>
+  )
+}
+
+export default ProjectsContainer
